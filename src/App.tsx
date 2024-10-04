0305c74f29c77b07472171552168dab6d0e0a83b
Safewaan <safwaanchowdhury26@gmail.com>
2024-10-04 16:14:27 -0400

    init frontend

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
