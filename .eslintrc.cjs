2d8bfdc264b91ea130629d3b6928f0de3143aab4
Safewaan <safwaanchowdhury26@gmail.com>
2024-11-01 14:06:59 -0400

    x

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,4 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
@@ -17,8 +6,26 @@ module.exports = {
     project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
+  env: { browser: true, es2020: true },
   plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@tanstack/query/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
   rules: {
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
     'react/prop-types': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
@@ -47,4 +54,5 @@ module.exports = {
   settings: {
     react: { version: 'detect' },
   },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
 };
