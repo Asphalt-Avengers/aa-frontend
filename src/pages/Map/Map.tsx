e497dea075e6b4f3c838c335e998dae52d4c306e
Safwaan Chowdhury <57375371+Safewaan@users.noreply.github.com>
2025-03-04 14:47:44 -0500

    sarcophagus ii (#10)

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,11 +1,14 @@
 import { Page, PageContent, PageDate, PageHeader } from '@/components/custom';
+import { GoogleMap } from '@/pages/Map/GoogleMap';
 
 export const Map: React.FC = () => {
   return (
     <Page>
       <PageDate />
       <PageHeader>Map</PageHeader>
-      <PageContent></PageContent>
+      <PageContent>
+        <GoogleMap />
+      </PageContent>
     </Page>
   );
 };
