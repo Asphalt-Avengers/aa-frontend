984040076ebea4d0c9a75a318177b72e19cba2b0
Safewaan <safwaanchowdhury26@gmail.com>
2025-03-03 20:39:32 -0500

    sarcophagus ii

---
+++ src/pages/Map/GoogleMap.tsx
@@ -0,0 +1,42 @@
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import React from 'react';
+
+import { useGetReports } from '@/hooks/report/useGetReports';
+
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY as string;
+
+export const GoogleMap: React.FC = () => {
+  const { data: reports, isSuccess } = useGetReports();
+
+  if (isSuccess) {
+    return (
+      <APIProvider
+        apiKey={GOOGLE_API_KEY}
+        onLoad={() => console.log('Maps API has loaded.')}
+      >
+        <div style={{ height: '75vh', width: '100%' }}>
+          <Map
+            zoom={14}
+            defaultCenter={{
+              lat: 43.4723,
+              lng: -80.5449,
+            }}
+          >
+            {reports.map((report) => {
+              const [latitude, longitude] = report.geom.split(',').map(Number);
+              return (
+                <Marker
+                  key={report.id}
+                  position={{ lat: latitude, lng: longitude }}
+                  onClick={() => console.log(report)}
+                />
+              );
+            })}
+          </Map>
+        </div>
+      </APIProvider>
+    );
+  }
+
+  return null;
+};
