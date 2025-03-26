0f8aa5a738b71d984e8a32f461e28a2ea895ac93
Connor Bechthold <bechtholdconnor@gmail.com>
2025-03-26 03:12:36 -0400

    fix overflow bug

--- src/components/custom/Page/Page.tsx
+++ src/components/custom/Page/Page.tsx
@@ -8,7 +8,7 @@ export const Page: React.FC<PageProps> = ({ className, children }) => {
   return (
     <div
       className={classNames(
-        'flex flex-col px-40 py-12 h-fit',
+        'flex flex-col px-10 py-12 h-fit',
         className,
         'Page-root'
       )}
